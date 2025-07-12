47504349ba22434bb414f8791a7037974a284503
AJAYJAITWAL <ajayjaitwal3434@gmail.com>
2025-07-12 11:01:15 +0530

    Fixed linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   before_action :authenticate_user!
@@ -14,7 +13,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:date_of_birth, :parental_consent_given])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:date_of_birth, :parental_consent_given])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :date_of_birth, :parental_consent_given ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :date_of_birth, :parental_consent_given ])
   end
 end
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,6 +1,6 @@
 class OrganizationsController < ApplicationController
-  before_action :set_organization, only: [:show, :analytics]
-  before_action :require_adult_user, only: [:new, :create]
+  before_action :set_organization, only: [ :show, :analytics ]
+  before_action :require_adult_user, only: [ :new, :create ]
 
   def index
     @organizations = Organization.all
