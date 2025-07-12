af5940d66be3419078d3abcbcf4009afadaca421
AJAYJAITWAL <ajayjaitwal3434@gmail.com>
2025-07-12 08:56:16 +0530

    Added required scope and methods

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,4 +3,10 @@ class Organization < ApplicationRecord
   has_many :users, through: :memberships
 
   validates :name, presence: true
+
+  scope :with_most_members, -> { joins(:memberships).group(:id).order('count(memberships.id) DESC') }
+
+  def admin_users
+    users.joins(:memberships).where(memberships: { role: "admin" })
+  end
 end
